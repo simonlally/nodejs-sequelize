c004b0c896a7ad7321dd9293b643b2a66331155f
simon lally <simonlallydev@gmail.com>
2020-05-01 14:36:49 -0400

    Add and initalize Sequelize in server.js

--- server.js
+++ server.js
@@ -4,15 +4,20 @@ const cors = require("cors");
 
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:4000",
-};
+// const corsOptions = {
+//   origin: "http://localhost:4000",
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./src/models");
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
 
 // test route
 app.get("/", (req, res) => {
