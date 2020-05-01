95aa2abb30036e319f51a2dd234b9d6a2e4ed031
simon lally <simonlallydev@gmail.com>
2020-05-01 10:01:19 -0400

    Setup server.js,, add project dependencies, add folder structure

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:4000",
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// test route
+app.get("/", (req, res) => {
+  res.json({ message: "Hello and welcome!" });
+});
+
+const port = process.env.port || 4000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
